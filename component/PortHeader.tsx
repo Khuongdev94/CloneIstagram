6a32dd74b2df9a9e478c0c00c1764a5e44431d45
khuong <khuonghumg1376@gmail.com>
2023-09-17 17:36:59 +0700

    update component PortHeader

--- component/PortHeader.tsx
+++ component/PortHeader.tsx
@@ -1,16 +1,5 @@
-import React, { useCallback, useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  TextInput,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableOpacity,
-  Alert,
-  Keyboard,
-} from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 import { Ionicons, Entypo } from "@expo/vector-icons";
 import Colors from "../contants/Colors";
 import FontSize from "../contants/FontSize";
@@ -40,60 +29,14 @@ export default function PortHeader(props: IProps) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 5,
-    marginVertical: 20,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-  textHead: {
-    fontSize: FontSize.h2,
-  },
-  blockIcon: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-  },
   iconPassword: {
     marginHorizontal: 6,
     padding: 10,
-  },
-  story: {
-    flexDirection: "row",
-    marginBottom: 24,
-  },
-  userName: {
-    fontSize: FontSize.h6,
-    textAlign: "center",
-  },
-  imgUser: {
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    resizeMode: "cover",
   },
   imgNews: {
     width: 30,
     height: 30,
     borderRadius: 15,
     resizeMode: "cover",
-  },
-  imgNewMain: {
-    width: "100%",
-    height: 460,
-    resizeMode: "cover",
-  },
-  icon: {
-    position: "absolute",
-    top: 34,
-    right: 5,
-    backgroundColor: "white",
-    padding: 2,
-    borderRadius: 16,
   },
 });
