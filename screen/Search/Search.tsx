4a0572d3ec4dcb2f88549030030f8c386c05fc3d
khuong <khuonghumg1376@gmail.com>
2023-09-17 17:44:33 +0700

    add feature Search

--- screen/Search/Search.tsx
+++ screen/Search/Search.tsx
@@ -6,305 +6,35 @@ import {
   TextInput,
   KeyboardAvoidingView,
   Platform,
-  TouchableOpacity,
-  Alert,
-  Keyboard,
   Image,
   FlatList,
 } from "react-native";
-import { Ionicons, FontAwesome5, Entypo, AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import Colors from "../../contants/Colors";
 import FontSize from "../../contants/FontSize";
+import { userStory } from "../db";
+
+interface FetchListPostParams {
+  keyword?: string;
+  isLike?: boolean;
+}
 
 export default function Search() {
   const [text, setText] = useState("");
+  const [dataFilter, setDataFilter] = useState([...userStory]);
   const handleGetText = (text: string) => {
     setText(text);
   };
-  const userStory = [
-    {
-      id: "1",
-      name: "tailor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-        numberLike: "2345 Likes",
-        numberComment: 34,
-        userComment: "thung",
-        contentComment: " Are your ok ?",
-      },
-    },
-    {
-      id: "2",
-      name: "David",
-      url: "https://www.acfc.com.vn/acfc_wp/wp-content/uploads/2021/07/cach-chup-anh-chan-dung-nam-3.jpeg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1367807801/vi/vec-to/h%E1%BB%95-d%E1%BB%85-th%C6%B0%C6%A1ng-ch%C3%A2n-dung-h%C3%ACnh-vu%C3%B4ng-c%C6%B0%E1%BB%9Di-%C4%91%E1%BA%A7u-ho%E1%BA%A1t-h%C3%ACnh-khu%C3%B4n-m%E1%BA%B7t-%C4%91%E1%BB%99ng-v%E1%BA%ADt-h%C3%ACnh-tr%C3%B2n-h%C3%ACnh-minh.jpg?s=1024x1024&w=is&k=20&c=2Q2aBKpflGyZ-PEwQFpBRyVyI79xH7yzA98mtnp_1g8=",
-        numberLike: "2344 Likes",
-        numberComment: 234,
-        userComment: "node",
-        contentComment: " Are your ok ? happy ?",
-      },
-    },
-    {
-      id: "3",
-      name: "Jame",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/324.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "4",
-      name: "Khuong",
-      url: "https://kieutruong.com/wp-content/uploads/2020/11/thong-so-chup-anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/579.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "5",
-      name: "sanches",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/908.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "6",
-      name: "Luis",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "7",
-      name: "thor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "8",
-      name: "Ant",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://phongvu.vn/cong-nghe/wp-content/uploads/2019/08/rule-of-thirds-composition-57dfc2d64ad27__880.jpg",
-        numberComment: 15,
-        numberLike: "129 Likes",
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "9",
-      name: "tailor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-        numberLike: "2345 Likes",
-        numberComment: 34,
-        userComment: "thung",
-        contentComment: " Are your ok ?",
-      },
-    },
-    {
-      id: "10",
-      name: "David",
-      url: "https://www.acfc.com.vn/acfc_wp/wp-content/uploads/2021/07/cach-chup-anh-chan-dung-nam-3.jpeg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1367807801/vi/vec-to/h%E1%BB%95-d%E1%BB%85-th%C6%B0%C6%A1ng-ch%C3%A2n-dung-h%C3%ACnh-vu%C3%B4ng-c%C6%B0%E1%BB%9Di-%C4%91%E1%BA%A7u-ho%E1%BA%A1t-h%C3%ACnh-khu%C3%B4n-m%E1%BA%B7t-%C4%91%E1%BB%99ng-v%E1%BA%ADt-h%C3%ACnh-tr%C3%B2n-h%C3%ACnh-minh.jpg?s=1024x1024&w=is&k=20&c=2Q2aBKpflGyZ-PEwQFpBRyVyI79xH7yzA98mtnp_1g8=",
-        numberLike: "2344 Likes",
-        numberComment: 234,
-        userComment: "node",
-        contentComment: " Are your ok ? happy ?",
-      },
-    },
-    {
-      id: "11",
-      name: "Jame",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/324.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "12",
-      name: "dung",
-      url: "https://kieutruong.com/wp-content/uploads/2020/11/thong-so-chup-anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/579.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "13",
-      name: "sanches",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/908.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "14",
-      name: "Luis",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "15",
-      name: "thor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "16",
-      name: "Ant",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://phongvu.vn/cong-nghe/wp-content/uploads/2019/08/rule-of-thirds-composition-57dfc2d64ad27__880.jpg",
-        numberComment: 15,
-        numberLike: "129 Likes",
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "17",
-      name: "tailor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-        numberLike: "2345 Likes",
-        numberComment: 34,
-        userComment: "thung",
-        contentComment: " Are your ok ?",
-      },
-    },
-    {
-      id: "18",
-      name: "David",
-      url: "https://www.acfc.com.vn/acfc_wp/wp-content/uploads/2021/07/cach-chup-anh-chan-dung-nam-3.jpeg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1367807801/vi/vec-to/h%E1%BB%95-d%E1%BB%85-th%C6%B0%C6%A1ng-ch%C3%A2n-dung-h%C3%ACnh-vu%C3%B4ng-c%C6%B0%E1%BB%9Di-%C4%91%E1%BA%A7u-ho%E1%BA%A1t-h%C3%ACnh-khu%C3%B4n-m%E1%BA%B7t-%C4%91%E1%BB%99ng-v%E1%BA%ADt-h%C3%ACnh-tr%C3%B2n-h%C3%ACnh-minh.jpg?s=1024x1024&w=is&k=20&c=2Q2aBKpflGyZ-PEwQFpBRyVyI79xH7yzA98mtnp_1g8=",
-        numberLike: "2344 Likes",
-        numberComment: 234,
-        userComment: "node",
-        contentComment: " Are your ok ? happy ?",
-      },
-    },
-    {
-      id: "19",
-      name: "Jame",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/324.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "20",
-      name: "dung",
-      url: "https://kieutruong.com/wp-content/uploads/2020/11/thong-so-chup-anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/579.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      id: "21",
-      name: "sanches",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/908.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-  ];
-  const [dataFilter, setDataFilter] = useState(userStory);
-  const filterUser = () => {
-    return userStory.filter((user) =>
-      user.name.toLowerCase().includes(text.toLowerCase())
+  const userFilter = () => {
+    return userStory.filter((item) =>
+      item.name.toLowerCase().includes(text.toLowerCase())
     );
   };
   useEffect(() => {
-    setDataFilter(filterUser());
+    const data = userFilter();
+    setDataFilter([...data]);
   }, [text]);
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -346,9 +76,10 @@ export default function Search() {
           <FlatList
             showsHorizontalScrollIndicator={false}
             data={dataFilter}
+            contentContainerStyle={styles.container}
             numColumns={3}
             renderItem={({ item }) => (
-              <View style={{ alignItems: "center", margin: 2 }}>
+              <View style={{ flex: 1, paddingVertical: 2 }}>
                 <Image
                   style={styles.imgUser}
                   source={{
@@ -366,7 +97,7 @@ export default function Search() {
               alignItems: "center",
             }}
           >
-            <Text style={{ fontSize: FontSize.h2 }}>Not Found</Text>
+            <Text style={{ fontSize: FontSize.h3 }}>Not Found</Text>
           </View>
         )}
       </View>
@@ -376,18 +107,8 @@ export default function Search() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    padding: 5,
-    marginVertical: 20,
+    paddingHorizontal: 2,
   },
-  header: {
-    flexDirection: "row",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    marginBottom: 16,
-  },
-
   blockIcon: {
     flexDirection: "row",
     justifyContent: "flex-end",
@@ -395,10 +116,6 @@ const styles = StyleSheet.create({
   iconPassword: {
     marginHorizontal: 6,
     padding: 10,
-  },
-  story: {
-    flexDirection: "row",
-    marginBottom: 24,
   },
   input: {
     color: Colors.secondary,
@@ -410,11 +127,6 @@ const styles = StyleSheet.create({
   imgUser: {
     width: 133,
     height: 133,
-    resizeMode: "cover",
-  },
-  imgNewMain: {
-    width: "100%",
-    height: 460,
     resizeMode: "cover",
   },
 });
