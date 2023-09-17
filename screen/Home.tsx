808f45cdd996be4bb017b9d0873721f3fb4d0938
khuong <khuonghumg1376@gmail.com>
2023-09-17 17:41:03 +0700

    add feature like

--- screen/Home.tsx
+++ screen/Home.tsx
@@ -1,128 +1,70 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  KeyboardAvoidingView,
-  Platform,
-  TouchableOpacity,
-  Alert,
-  Keyboard,
-  Image,
-  FlatList,
-} from "react-native";
-import { Ionicons, FontAwesome5, Entypo, AntDesign } from "@expo/vector-icons";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View, Image, FlatList } from "react-native";
+import { Ionicons, FontAwesome5, AntDesign } from "@expo/vector-icons";
 
-import Colors from "../contants/Colors";
 import FontSize from "../contants/FontSize";
 import Describe from "../component/Describe";
 import PortHeader from "../component/PortHeader";
-import Interact from "../component/Interact";
 import Header from "../component/Header";
+import { userStory } from "./db";
+
+interface FetchListPostParams {
+  keyword?: string;
+  isLike?: boolean;
+  isGetOnlyLike?: boolean;
+}
+
+function fetchUserStory(params: FetchListPostParams) {
+  const userIsLike = userStory.filter((item) => {
+    if (params.keyword && !item.name.includes(params.keyword)) return false;
+    if (params.isLike !== undefined && item.isLike !== params.isLike) {
+      return false;
+    }
+    if (params.isGetOnlyLike === true && item.isLike === false) {
+      return false;
+    }
+    return true;
+  });
+  return userIsLike;
+}
+
 export default function Home() {
-  const userStory = [
-    {
-      name: "tailor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-        numberLike: "2345 Likes",
-        numberComment: 34,
-        userComment: "thung",
-        contentComment: " Are your ok ?",
-      },
-    },
-    {
-      name: "David",
-      url: "https://www.acfc.com.vn/acfc_wp/wp-content/uploads/2021/07/cach-chup-anh-chan-dung-nam-3.jpeg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1367807801/vi/vec-to/h%E1%BB%95-d%E1%BB%85-th%C6%B0%C6%A1ng-ch%C3%A2n-dung-h%C3%ACnh-vu%C3%B4ng-c%C6%B0%E1%BB%9Di-%C4%91%E1%BA%A7u-ho%E1%BA%A1t-h%C3%ACnh-khu%C3%B4n-m%E1%BA%B7t-%C4%91%E1%BB%99ng-v%E1%BA%ADt-h%C3%ACnh-tr%C3%B2n-h%C3%ACnh-minh.jpg?s=1024x1024&w=is&k=20&c=2Q2aBKpflGyZ-PEwQFpBRyVyI79xH7yzA98mtnp_1g8=",
-        numberLike: "2344 Likes",
-        numberComment: 234,
-        userComment: "node",
-        contentComment: " Are your ok ? happy ?",
-      },
-    },
-    {
-      name: "Jame",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/324.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      name: "dung",
-      url: "https://kieutruong.com/wp-content/uploads/2020/11/thong-so-chup-anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/579.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      name: "sanches",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/908.jpg",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      name: "Luis",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      name: "thor",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://media.istockphoto.com/id/1159885960/vi/vec-to/phong-c%C3%A1ch-v%E1%BA%BD-tay-ho%E1%BA%A1t-h%C3%ACnh-c%C3%A1-s%E1%BA%A5u.jpg?s=1024x1024&w=is&k=20&c=bn-wnRiqoJDwatsKq-65ulu14A-ZjZkVimMHDc2XQew=",
-        numberLike: "129 Likes",
-        numberComment: 15,
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-    {
-      name: "Ant",
-      url: "https://mayanhhoangto.com/wp-content/uploads/2022/05/anh-chan-dung.jpg",
-      describe: {
-        urlPort:
-          "https://phongvu.vn/cong-nghe/wp-content/uploads/2019/08/rule-of-thirds-composition-57dfc2d64ad27__880.jpg",
-        numberComment: 15,
-        numberLike: "129 Likes",
-        userComment: "npm npx",
-        contentComment: " Are your ok ? Close ?",
-      },
-    },
-  ];
+  const [stories, setStories] = useState([...userStory]);
+  const [like, setLike] = useState(true);
+
+  const handleLike = (id: string) => {
+    const story = stories.find((item) => item.id === id);
+    if (!story) {
+      console.warn("handleLike: Story not found");
+      return;
+    }
+    story.isLike = !story.isLike;
+    setStories([...stories]);
+  };
+
+  useEffect(() => {
+    const data = fetchUserStory({ isGetOnlyLike: like });
+    setStories([...data]);
+  }, [like]);
+
+  const handleFilterLike = (isGetOnlyLike: boolean) => {
+    setLike(isGetOnlyLike);
+  };
+
+  const handleFilterUserLike = (isLike: boolean) => {
+    const storiesLiked = stories.filter((item) => item.isLike === true);
+    if (isLike === true) {
+      setStories([...storiesLiked]);
+    } else {
+      setStories([...userStory]);
+    }
+    setLike(isLike);
+  };
+
   return (
     <View style={styles.container}>
       {/* header */}
-      <Header />
+      <Header onFilterLike={() => handleFilterLike(!like)} like={like} />
       {/* story */}
       <View style={styles.story}>
         <View style={{ marginRight: 5 }}>
@@ -145,8 +87,16 @@ export default function Home() {
           />
           <Text style={styles.userName}>Your Story</Text>
         </View>
-        <View style={{ flex: 1 }}>
+        <View
+          style={{
+            flex: 1,
+            borderBottomLeftRadius: 40,
+            borderTopLeftRadius: 40,
+            overflow: "hidden",
+          }}
+        >
           <FlatList
+            showsHorizontalScrollIndicator={false}
             data={userStory}
             horizontal={true}
             renderItem={({ item }) => (
@@ -160,13 +110,13 @@ export default function Home() {
                 <Text style={styles.userName}>{item.name}</Text>
               </View>
             )}
-            keyExtractor={(item) => item.name}
+            keyExtractor={(item) => item.id}
           />
         </View>
       </View>
       {/* bảng tin */}
       <FlatList
-        data={userStory}
+        data={stories}
         renderItem={({ item }) => (
           <View style={{ marginBottom: 18 }}>
             <PortHeader url={item.url} name={item.name}></PortHeader>
@@ -178,7 +128,39 @@ export default function Home() {
                 }}
               />
             </View>
-            <Interact />
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "center",
+                paddingHorizontal: 6,
+              }}
+            >
+              <Ionicons
+                style={styles.iconPassword}
+                name={item.isLike ? "heart-sharp" : "heart-outline"}
+                size={26}
+                color={item.isLike ? "red" : "black"}
+                onPress={() => handleLike(item.id)}
+              />
+              <AntDesign
+                style={(styles.iconPassword, { textAlign: "right" })}
+                name="message1"
+                size={22}
+                color={"black"}
+              />
+              <FontAwesome5
+                style={styles.iconPassword}
+                name="telegram-plane"
+                size={26}
+                color={"black"}
+              />
+              <Ionicons
+                style={(styles.iconPassword, { flex: 1, textAlign: "right" })}
+                name="bookmarks-outline"
+                size={22}
+                color={"black"}
+              />
+            </View>
             <Describe
               numberLike={item.describe.numberLike}
               numberComment={item.describe.numberComment}
